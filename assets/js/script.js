5f411b6698507e845b2071ac7ea499fa730173ef
Ben Robuck <brobuck@apple.com>
2022-08-08 12:13:28 -0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var timerEl = $("#timerEl")
+var todaysDate = ""
+
+function refreshDate(){
+    todaysDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    timerEl.text(todaysDate);
+}
+
+function refreshTimer(){
+        setInterval(refreshDate(), 1000);
+      }
+
+window.addEventListener('load', (event) => {
+    refreshDate();
+    refreshTimer();
+  });
