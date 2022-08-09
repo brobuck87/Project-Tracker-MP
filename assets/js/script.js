32ab39558083e8fdc49fc937fb359c78b68f4964
Ben Robuck <brobuck@apple.com>
2022-08-09 14:23:15 -0400

    Add modal. Fix for js to refresh time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,24 @@
 var timerEl = $("#timerEl")
 var todaysDate = ""
+var openForm = $("open-form")
+var modalShow = $('#exampleModal').
+
+openForm.click(function(){
+  modalShow.show();
+})
+
+
 
 function refreshDate(){
     todaysDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-    timerEl.text(todaysDate);
-}
+    timerEl.text(todaysDate)
+};
 
 function refreshTimer(){
-        setInterval(refreshDate(), 1000);
+        setInterval(refreshDate, 1000)
       }
 
 window.addEventListener('load', (event) => {
-    refreshDate();
-    refreshTimer();
-  });
+    refreshDate()
+    refreshTimer()
+  })
