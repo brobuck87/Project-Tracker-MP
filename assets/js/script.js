ed5dd540b4041deb0a63af0d2a45cd7b27ff57b4
Ben Robuck <brobuck87@gmail.com>
2022-08-12 10:36:06 -0400

    remove th to make table cleaner. Format of text files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-var projectForm = $("#project-form")
+var projectForm = $("#project-form");
 
 function refreshDate() {
   var todaysDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-  var timerEl = $("#timerEl")
-  timerEl.text(todaysDate)
-};
+  var timerEl = $("#timerEl");
+  timerEl.text(todaysDate);
+}
 
 function refreshTimer() {
-  setInterval(refreshDate, 1000)
+  setInterval(refreshDate, 1000);
 }
 
 function handleFormSubmit(event) {
@@ -15,41 +15,46 @@ function handleFormSubmit(event) {
   //calculate difference between and date-picker.val
   var current = moment();
   var selectedDate = moment($("#date-picker").val(), "MM/DD/YYYY");
-  var untilDue = selectedDate.diff(current, 'days')
-  var estimatedEarnings = untilDue * ($('#hourly-wage').val() * 8);
-  var tableInfo = [$("#project-name").val(), $("#project-selector").val(), $("#hourly-wage").val(), $("#date-picker").val(), untilDue, estimatedEarnings];
-  var tableRow = $('<tr>')
-  
-  for (let i = 0; i < tableInfo.length; i++){
-     //create td
-     var tableData = $('<td>')
+  var untilDue = selectedDate.diff(current, "days");
+  var estimatedEarnings = untilDue * ($("#hourly-wage").val() * 8);
+  var tableInfo = [
+    $("#project-name").val(),
+    $("#project-selector").val(),
+    $("#hourly-wage").val(),
+    $("#date-picker").val(),
+    untilDue,
+    estimatedEarnings,
+  ];
+  var tableRow = $("<tr>");
 
-     //set text to be current value
-     tableData.text(tableInfo[i]);
+  for (let i = 0; i < tableInfo.length; i++) {
+    //create td
+    var tableData = $("<td>");
 
-     //put in tr
-     tableRow.append(tableData);
-     };
-     var buttonTD = $('<td>')
-     var deleteButton = $('<button>');
-     deleteButton.attr('type', 'button') 
-     deleteButton.attr('class', 'btn btn-danger delete fa fa-trash') 
-     buttonTD.append(deleteButton)
-     tableRow.append(buttonTD)
-  $('#project-info tbody').on('click', '.delete',function(){
-    $(this).closest('tr').remove();
-  })
-  $('#project-info tbody').append(tableRow);
-  projectForm.trigger('reset');
-  $('#formModal').modal('hide');
+    //set text to be current value
+    tableData.text(tableInfo[i]);
+
+    //put in tr
+    tableRow.append(tableData);
+  }
+  var buttonTD = $("<td>");
+  var deleteButton = $("<button>");
+  deleteButton.attr("type", "button");
+  deleteButton.attr("class", "btn btn-danger delete fa fa-trash");
+  buttonTD.append(deleteButton);
+  tableRow.append(buttonTD);
+  $("#project-info tbody").on("click", ".delete", function () {
+    $(this).closest("tr").remove();
+  });
+  $("#project-info tbody").append(tableRow);
+  projectForm.trigger("reset");
+  $("#formModal").modal("hide");
 }
 
-
-
-window.addEventListener('load', (event) => {
-  refreshDate()
-  refreshTimer()
+window.addEventListener("load", (event) => {
+  refreshDate();
+  refreshTimer();
   $("#date-picker").datepicker();
-})
+});
 
-projectForm.on('submit', handleFormSubmit);
+projectForm.on("submit", handleFormSubmit);
